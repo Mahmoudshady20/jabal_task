8ef3118d7ebcd1e9ba87932f4b547464da82b51f
user <mahmoud.enshagshan0@gmail.com>
2025-04-02 08:16:13 +0200

    1- finish heart icon

--- lib/features/product_details_feature/presentation/view/widget/product_details_body.dart
+++ lib/features/product_details_feature/presentation/view/widget/product_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:jabal_task/features/home_feature/presentation/views/widgets/order_button.dart';
 import 'package:jabal_task/features/product_details_feature/presentation/view/widget/burger_image_widget.dart';
 import 'package:jabal_task/features/product_details_feature/presentation/view/widget/descreption_column.dart';
@@ -9,20 +10,35 @@ class ProductDetailsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(
         children: [
-          BurgerImageWidget(),
-          DescriptionColumn(),
-          SizedBox(
-            height: 3,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BurgerImageWidget(),
+              DescriptionColumn(),
+              SizedBox(
+                height: 3,
+              ),
+              Divider(
+                color: Color(0xFF707070).withAlpha(60),
+                thickness: .5,
+              ),
+              OrderButton(),
+            ],
           ),
-          Divider(
-            color: Color(0xFF707070).withAlpha(60),
-            thickness: .5,
-          ),
-          OrderButton(),
+          Positioned(
+              top: MediaQuery.sizeOf(context).height * .385,
+              right: MediaQuery.sizeOf(context).width * .17,
+              child: CircleAvatar(
+                backgroundColor: Color(0xFFF1395E),
+                radius: 25,
+                child: FaIcon(
+                  FontAwesomeIcons.heart,
+                  color: Colors.white,
+                ),
+              )),
         ],
       ),
     );
